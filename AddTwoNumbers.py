d3c983f7b9716dfc20cf56f1a84ba19f1829f2e6
Logan Ankenbrandt <logan.ankenbrandt@addepar.com>
2021-08-13 11:23:05 -0500

    August 13: Having issues with passing the list through the necessary functions.
    1. Issues
        - Currently receiving these errors:
            a. print(addTwoNumbers(None, [2, 3, 4], [5, 6, 4]))
            b. list_1 = convertList(l1)
            c. len = find_length(head)
            d. curr = curr.next
            e. AttributeError: 'list' object has no attribute 'next'
        - I believe the issue is that the lists that are being passed
        through are not actually linked lists, so they can't be passed
        through my functions.
    2. Solution
        - Iteratively convert the lists to linked lists
        - Pass the linked lists through the functions

--- AddTwoNumbers.py
+++ AddTwoNumbers.py
@@ -1,8 +1,39 @@
-# Definition for singly-linked list.
+
 class ListNode(object):
     def __init__(self, x):
+        """
+        This is the function to initialize
+        a node for a singly linked list.
+        """
         self.val = x
         self.next = None
+
+
+def find_length(head):
+    """
+    This is the function to find the length
+    of a linked list.
+    """
+    curr = head
+    count = 0
+    while curr is not None:
+        count += 1
+        curr = curr.next
+    return count
+
+def convertList(head):
+    """
+    This is the function to convert the
+    linked list to a number.
+    """
+    len = find_length(head)
+    arr = []
+    curr = head
+    while curr is not None:
+        arr.append(curr.val)
+        curr = curr.next
+   
+    return arr, len
 
 def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
         """
@@ -47,5 +78,25 @@ def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
                         - 9999999 + 9999
                     ii. Output
                         - 9999999 + 9999 = 89990001
-                
+        3. Params:
+            - l1: Linked List
+            - l2: Linked List
+        4. Implementation
         """
+        list_1 = convertList(l1)
+        list_2 = convertList(l2)
+        print(list_1, list_2)
+
+print(convertList(self=None, l1=[2, 3, 4], l2=[5, 6, 4]))
+
+"""
+1. Issues
+    - Currently receiving these errors:
+        a. print(addTwoNumbers(None, [2, 3, 4], [5, 6, 4]))
+        b. list_1 = convertList(l1)
+        c. len = find_length(head)
+        d. curr = curr.next
+        e. AttributeError: 'list' object has no attribute 'next'
+    - I believe the issue is that the lists that are being passed through,
+
+"""
