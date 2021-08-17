420465bff33160d392bea355ccc754c9706ea701
Logan Ankenbrandt <logananks@peoplepc.com>
2021-08-17 17:24:06 -0500

    August 17: Initialized MaxiumumWealth.py
    1. Implementation
                - Initiailze an empty list to store the
                sum of the lists
                - Iterate through the list of lists
                - Append the sum of the lists to the list
                - Return the max of the list

---
+++ MaximumWealth.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+def maximumWealth(accounts: List[List[int]]) -> int:
+        """
+        1. Goal
+            - Iterate through a list of lists to find the 
+            greatest sum
+        2. Examples
+            - [[1,2,3],[4,5,6],[7,8,9]] -> 15
+        3. Implementation
+            - Initiailze an empty list to store the 
+            sum of the lists
+            - Iterate through the list of lists
+            - Append the sum of the lists to the list
+            - Return the max of the list
+        """
+        res = []
+        for i in accounts:
+            res.append(sum(i))
+        return max(res)
