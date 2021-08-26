2dd46f01d8a4ac3f9334b387b826420a8fe8619c
logan.ankenbrandt <logananks@peoplepc.com>
2021-08-26 11:01:26 -0500

    Aug 26: Initialized ReverseString.py
    1. Implementation
            a. Steps
                1. Use the reverse() function to reverse
                the elements in the list

---
+++ ReverseString.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+def reverseString(self, s: List[str]) -> None:
+    """
+    Do not return anything, modify s in-place instead.
+    1. Goal
+        a. Reverse the order of the characters in s
+    2. Implementation
+        a. Steps
+            1. Use the reverse() function to reverse
+            the elements in the list
+    """
+    s.reverse()
